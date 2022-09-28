c6cf6e307e9fb23da481a5400412e47e8c1d42db
Le Mong Thuy <thuyle2000@gmail.com>
2022-09-28 08:38:45 +0700

    Upload Sep 28, 2022

--- SQL/d05_Avanced_SELECT.sql
+++ SQL/d05_Avanced_SELECT.sql
@@ -3,6 +3,7 @@
 USE  db2205M0
 GO
 
+
 --1. in ds sinh vien co diem thi mon LBEP cao nhat
 -- cach 1: ap dung select top 1 with ties
 SELECT * FROM tbExam 
@@ -53,3 +54,59 @@ SELECT * FROM tbStudent
 					WHERE module_id LIKE (SELECT id FROM tbModule 
 													WHERE module_name LIKE '%angular%'))
 GO
+
+
+
+
+--4.  xem ket qua thi mon C bao gom ho ten sinh vien : ap dung phep ket JOIN
+SELECT diem.*, sv.fullname
+	FROM tbExam [diem] JOIN tbStudent [sv] ON diem.student_id = sv.id
+	WHERE module_id LIKE 'LBEP'
+
+SELECT diem.id,  diem.student_id, sv.fullname, diem.mark
+	FROM tbExam [diem] JOIN tbStudent [sv] ON diem.student_id = sv.id
+	WHERE module_id LIKE 'LBEP'
+
+
+--5. xem ds sinh vien chua thi mon C
+-- ds sv da thi mon C
+SELECT * FROM tbStudent 
+	WHERE id IN (SELECT DISTINCT student_id FROM tbExam 
+					WHERE module_id LIKE (SELECT id FROM tbModule 
+													WHERE module_name LIKE '%lap trinh C%'))
+
+SELECT DISTINCT sv.*
+	FROM (tbStudent [sv] JOIN tbExam [diem] ON sv.id = diem.student_id) 
+						 JOIN tbModule [mon] ON mon.id = diem.module_id
+	WHERE mon.module_name LIKE '%lap trinh C%'
+GO
+
+
+-- ds sv chua thi mon C
+-- ds sv da thi (mark <> null) va chua thi mon C (mark=null)
+SELECT  d.fullname, c.*  from tbStudent [d] LEFT JOIN  
+	(SELECT a.id, b.module_name,  a.mark, a.student_id 
+		FROM tbExam [a] JOIN tbModule [b] ON a.module_id = b.id
+			WHERE module_name LIKE '%Lap trinh C%') [c]
+	ON d.id = c.student_id
+	ORDER BY c.mark
+
+-- ds sv chua thi mon C (mark=null)
+SELECT  d.fullname, c.*  from tbStudent [d] LEFT JOIN  
+	(SELECT a.id, b.module_name,  a.mark, a.student_id 
+		FROM tbExam [a] JOIN tbModule [b] ON a.module_id = b.id
+			WHERE module_name LIKE '%Lap trinh C%') [c]
+	ON d.id = c.student_id
+	WHERE c.mark is NULL
+	ORDER BY c.mark
+
+-- ds sv da thi mon C (mark=null)
+SELECT DISTINCT d.fullname,c.module_name,  c.student_id  
+	from tbStudent [d] LEFT JOIN  
+	(SELECT b.module_name,  a.mark, a.student_id 
+		FROM tbExam [a] JOIN tbModule [b] ON a.module_id = b.id
+			WHERE module_name LIKE '%Lap trinh C%') [c]
+	ON d.id = c.student_id
+	WHERE c.mark is NOT NULL
+
+
