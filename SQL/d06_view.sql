d8e1895f1a2560e415abbe9f2f0d103d22977dbd
Le Mong Thuy <thuyle2000@gmail.com>
2022-09-28 10:44:55 +0700

    Upload Sep 28, 2022

--- SQL/d06_view.sql
+++ SQL/d06_view.sql
@@ -19,3 +19,16 @@ GO
 
 -- xem ds sinh vien nu
 SELECT * FROM vwSchoolGirl
+GO
+
+--4. tao view chua chua ket qua thi, bao gom ho ten sv va ten mon thi [vwExam]: noi 3 bang tbExam, tbStudent va tbModule
+CREATE VIEW [vwExam] AS
+	SELECT a.id, 
+		   a.student_id, b.fullname, a.module_id, c.module_name, 
+		   a.mark
+		   FROM tbExam [a] JOIN tbStudent [b] ON a.student_id=b.id
+					       JOIN tbModule [c] ON a.module_id= c.id
+GO
+
+-- test view: xem ket qua thi voi day du thong tin ho ten va ten mon thi
+SELECT * FROM vwExam
