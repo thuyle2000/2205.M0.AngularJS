8c6882fc4d5ba4e5752d661e407bcc1b17dd02d9
Le Mong Thuy <thuyle2000@gmail.com>
2022-09-30 09:25:30 +0700

    Upload Sep 30, 2022

--- SQL/d07_store_procedure.sql
+++ SQL/d07_store_procedure.sql
@@ -1,4 +1,4 @@
--- test extended procedure
+/*  Demo extended procedure  */
 exec xp_logininfo
 GO
 
@@ -10,3 +10,36 @@ go
 
 exec xp_dirtree 'F:\student\2205-M0'
 go
+
+
+/*  
+ *  Demo custom (user-defined) store procedure  
+ */
+ -- open database [db2205M0] de lam viec
+ use db2205M0
+ go
+
+ /*
+	1. tao stored procedure [up_Student], thuc hien :
+		- in ra ds sv theo thu tu gioi tinh va ma so
+		- dem va in ra so sv nam , nu
+*/
+CREATE PROCEDURE up_Student AS
+BEGIN
+    -- in ra ds sv theo thu tu gioi tinh va ma so
+	SELECT * FROM tbStudent ORDER BY gender, id
+
+	-- dem va in ra so luong sv nam , nu
+	SELECT CASE gender WHEN 1 THEN 'Male'
+					   ELSE 'Female'
+		   END 'gender',			    
+		   COUNT(*) [total of students ] 
+	   FROM tbStudent GROUP BY gender
+
+END
+GO
+-- Test case:
+exec up_Student
+GO
+
+
