d8e3062ae71c679ab67f7751c332f00e07875c72
Le Mong Thuy <thuyle2000@gmail.com>
2022-10-03 11:46:04 +0700

    Upload Oct 3 2022

--- SQL/d08_index.sql
+++ SQL/d08_index.sql
@@ -15,3 +15,25 @@ SELECT * FROM tbStudent
 -- tao non-clustered index tren cot ho ten sinh vien va ngay sinh
 CREATE INDEX [ixStudentName] ON tbStudent(fullname, dob DESC)
 GO
+
+-- xem ds lop hoc
+select * FROM tbBatch
+
+-- xem ds mon hoc
+SELECT * FROM tbModule
+
+-- tao unique index tren cot ten mon hoc cua bang mon hoc : vi muon ten mon hoc ko duoc trung nhau
+CREATE UNIQUE INDEX [ixModuleName] ON tbModule(module_name)
+GO
+
+-- test case 1: them mon hoc moi (XML,'XML va JSON') : OK
+INSERT tbModule VALUES ('XML', 'XML & JSON', 20, 200)
+GO
+
+-- test case 2: them mon hoc moi (PRJ2,'Eproject I') : LOI vi ten mon hoc trung nhau !
+INSERT tbModule VALUES ('PRJ2', 'Eproject I', 24, 250)
+GO
+
+
+
+
